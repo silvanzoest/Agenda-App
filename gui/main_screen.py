bad218d1e4a4de937549be17a0c53d5d0a985090
Animajosser <animajosser@gmail.com>
2017-04-09 20:10:23 +0200

    Update main_screen.py

--- gui/main_screen.py
+++ gui/main_screen.py
@@ -1,1 +1,11 @@
+"""
+Dit is het bestand voor het algemene scherm van de Agenda-App
+Het heeft een aantal functies en dezen staat beschreven in de drive.
 
+<LICENSE>
+<COPYRIGHT NOTICE>
+<Animajosser>
+"""
+__version__="0.0"
+__date__="09-04-2017"
+##############################################################################################
