c0b85bf39a6db15d97bb6ac776a178fa305fde39
Animajosser <animajosser@gmail.com>
2017-04-09 20:08:21 +0200

    Update create_appointment.py

--- gui/create_appointment.py
+++ gui/create_appointment.py
@@ -1,5 +1,5 @@
 """
-Dit is het bestand voor het maken van afspraken scherm voor de Agenda-App
+Dit is het bestand voor het maken of aanpassen van afspraken scherm voor de Agenda-App
 Het heeft een aantal functies en dezen staat beschreven in de drive.
 
 <LICENSE>
