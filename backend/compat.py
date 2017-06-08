5fc22772c2502e2184fb87656fefb60a3edea5cd
silvanzoest <silvanzoest@hotmail.com>
2017-06-08 16:15:22 +0200

    Sil - Compatibiliteit
    Checkt of nodige keys aanwezig zijn in gegeven dictionary.
    Nog gebeuren: Checken of waarden keys overeenkomt met benodigde waarden.

--- backend/compat.py
+++ backend/compat.py
@@ -1,1 +1,13 @@
+# Werk van Sil
 
+# for-loop
+# [] 
+
+
+def validate_appointment(data: dict):
+    KeyList = ['title', 'description', ...]
+    for key in KeyList:
+        if key not in dict:
+            return False
+        else:
+            continue
